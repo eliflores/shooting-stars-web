db85dfc44bad0fef8e0be3bf5b0a0323f0abc9d5
mignonnesaurus <elifloresch@gmail.com>
2017-10-14 20:39:04 +0200

    Change location of static files to a public folder to be served by our node server

--- app/server.js
+++ app/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:3000
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
