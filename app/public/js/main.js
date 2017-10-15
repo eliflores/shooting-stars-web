3300740d320d680946dff179b976d6b9423cda28
mignonnesaurus <elifloresch@gmail.com>
2017-10-15 11:34:04 +0200

    Clean up javascript.

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     L.tileLayer(
         'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; ' + mapLink + ' Contributors',
-            maxZoom: 18,
+            maxZoom: 18
         }).addTo(map);
 
     L.Control.geocoder().addTo(map);
@@ -30,7 +30,6 @@ $(document).ready(function () {
     var loc = map.on('click', function (e) {
 
         if (userMarkers && userMarkers.length) {
-            console.log(userMarkers.length)
             //not empty
             for (var i = 0; i < userMarkers.length; i++) {
                 map.removeLayer(userMarkers[i]);
@@ -73,10 +72,7 @@ $(document).ready(function () {
             },
             dataType: 'json',
             success: function (data) {
-                console.log("data.results.length = " + data.length)
                 for (var i = 0; i < data.length; i++) {
-                    console.log("lat=" + data[i].latitude);
-                    console.log("lon=" + data[i].longitude);
                     var marker = new L.marker([data[i].latitude, data[i].longitude], {icon: greenIcon})
                         .bindPopup(data[i].date)
                         .addTo(map);
